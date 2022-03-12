052582bab77e09ca7f17e9ed2023eb186fcc93fe
justin <chenyouduan@gmail.com>
2022-03-12 22:50:23 +0800

    only pluse

---
+++ caculator1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 17:10:58 2022
+
+@author: cheny
+"""
+num1=input("請輸入第一個數字")
+num2=input("請輸入第二個數字")
+print("你要做怎樣的運算?\n加法按+，減法按-，乘法按*，除法按/")
+a=input()
+if a='+':
+    print(num1*num2)
+elif a='-':
+    print()
+elif a='*'
+    print()
+elif a='/'
